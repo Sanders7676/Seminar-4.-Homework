b0616488347440a2a34371f36bf044e0a45e03d5
Sanders76 <sanders76@list.ru>
2022-12-06 22:54:51 +0300

    Added code to Task_3_29

--- Task_3_29/Program.cs
+++ Task_3_29/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] arr = new int[8];
+
+int[] array = FillArray(arr);
+
+Console.WriteLine("Массив выглядит следующим образом: ");
+
+PrintArray (array);
+
+
+
+//метод заполнения массива
+
+int[] FillArray(int[] arr)
+{
+	Console.WriteLine("Введите числа для формирования массива:");
+
+    for (int i = 0; i < 8; i++)
+    {
+		Console.Write($"Введите {i+1}-e числo: ");
+	    int num = int.Parse(Console.ReadLine());
+		arr[i] = num;
+    }
+    return arr;
+}
+
+
+//Метод печати массива
+
+void PrintArray (int[] array)
+{
+	for (int i = 0; i < 8; i++)
+	{
+        Console.Write($"{array[i]}, ");
+	}
+}
+
