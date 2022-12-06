929145149e047260c5bc6cc54d58d3eb3bd20d47
Sanders76 <sanders76@list.ru>
2022-12-06 22:37:50 +0300

    Changes made to the code of Task_2_27

--- Task_2_27/Program.cs
+++ Task_2_27/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 27
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11 ; 82 -> 10 ; 9012 -> 12
+
+
+Console.Write("Введите число: ");
+
+int number = InputInt();
+
+int sumOfDigitsInNumber = SumOfDigitsInNumber(number);
+
+Console.Write($"Сумма цифр в числе {number} равна {sumOfDigitsInNumber}.");
+
+
+//Метод для приема чисел, вводимых через консоль
+
+int InputInt()
+{
+    bool isNum = int.TryParse(Console.ReadLine(), out int num);
+    if (isNum)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное значение.");
+        return -1;
+    }
+}
+
+
+// Метод нахождения суммы цифр в числе
+
+
+int SumOfDigitsInNumber(int number)
+{
+    int result = 0;
+
+    for (int temp = number; temp > 0; temp = temp / 10)
+    {
+        result = result + temp % 10;
+    }
+
+    return result;
+}
